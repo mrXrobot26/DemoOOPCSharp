f318a0e0464f3d623a1c64508ca0934343628582
Abdo Elbessa <zezoelbessa10@egyeagles.com>
2023-10-29 02:16:21 +0200

    Add project files.

---
+++ DemoOOP2/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoOOP2
+{
+    public class Car
+    {
+        public string Color;
+        public string name;
+        public string Brand;
+
+        public void Move()
+        {
+            Console.WriteLine("car is moving right now");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("car Stopped");
+        }
+
+        public void speedUp()
+        {
+            Console.WriteLine("car Speeding up Now");
+        }
+        public void speedDown()
+        {
+            Console.WriteLine("car Speeding Down Now");
+        }
+
+
+
+
+
+    }
+}
---
+++ DemoOOP2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace DemoOOP2
+{
+    internal class Program
+    {
+        //static int number(int num1 , int num2)
+        //{
+        //    int res = num1 + num2;
+        //    return res;
+        //}
+        static void Main(string[] args)
+        {
+            //int num = number(3, 4);
+            //Console.WriteLine(num);
+            //++num;
+            //Console.WriteLine(num);
+            //int[] arr = new int[10];
+
+
+
+            Car car1 = new Car();
+
+            car1.Color = "red";
+            car1.Brand = "BMW";
+            car1.name = "x7";
+
+            Console.WriteLine($" {car1.Brand} {car1.name} {car1.Color}");
+
+            Car car2 = new Car();
+
+            car2.Color = "AAAAAAAA";
+            car2.Brand = "AAAAAAAA";
+            car2.name =  "AAAAAAAA";
+
+            Console.WriteLine($" {car2.Brand} {car2.name} {car2.Color}");
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
